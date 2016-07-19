385b535b26be50e24b18e53fb84629f80ef43e3d
ipedronic <ipedronic@gmail.com>
2016-07-19 22:23:52 +0100

    primera subida
    Subo por primera vez

---
+++ PuertoSerie/PuertoSerie.ino
@@ -0,0 +1,27 @@
+//www.elbrujitocanaio.es 
+//Todo el código esta bajo licencia de la Creative Commons.
+#include <SoftwareSerial.h>
+char dato_puerto;
+
+SoftwareSerial ESP(3, 2); // RX | TX
+
+void setup()
+  {  Serial.begin(9600);
+     ESP.begin(9600);
+    Serial.println("Resetear el ESP8266,un pulso patilla reset a negativo");
+    Serial.println("Programar cuando aparece la palabra: ready"); 
+  }
+
+void loop()  { 
+  
+              
+     if (ESP.available()){
+           dato_puerto = ESP.read() ;          
+           Serial.print(dato_puerto);
+          }
+          
+     if (Serial.available()) {         
+          leer_puerto = Serial.read();
+          ESP.print(leer_puerto);
+         }
+   }
